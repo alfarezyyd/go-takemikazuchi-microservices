4da8f53297dfcd2d7045484190d460234a7e3ca2
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-04 23:20:31 +0700

    feat: general helper

---
+++ helper/general.go
@@ -0,0 +1,53 @@
+package helper
+
+import (
+	"fmt"
+	"go-takemikazuchi-api/exception"
+	"math/rand"
+	"reflect"
+	"strconv"
+)
+
+func CheckErrorOperation(indicatedError error, clientError *exception.ClientError) bool {
+	if indicatedError != nil {
+		panic(clientError)
+		return true
+	}
+	return false
+}
+
+func ParseNullableValue(value interface{}) interface{} {
+	v := reflect.ValueOf(value)
+
+	// Handle pointer types
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	// Check for sql.Null* types
+	if v.Kind() == reflect.Struct {
+		validField := v.FieldByName("Valid")
+		valueField := v.FieldByName("String") // Default to String; change for other nullable types
+
+		if validField.IsValid() && validField.Kind() == reflect.Bool {
+			if validField.Bool() {
+				return valueField.Interface()
+			}
+			return nil
+		}
+	}
+
+	// If not a nullable type, return the value as is
+	return value
+}
+
+func GenerateOneTimePasswordToken() string {
+	num := rand.Intn(9000) + 1000
+	return strconv.Itoa(num)
+}
+
+func LogError(err error) {
+	if err != nil {
+		fmt.Println("error occurred", err)
+	}
+}
