d84be2fb98cb3563057fedfd7cff4940f8e20a69
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-04 23:20:25 +0700

    feat: injection with DI

--- injector.go
+++ injector.go
@@ -2,3 +2,52 @@
 // +build wireinject
 
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	"github.com/google/wire"
+	"github.com/spf13/viper"
+	"go-takemikazuchi-api/config"
+	"go-takemikazuchi-api/routes"
+	"go-takemikazuchi-api/user"
+	"gorm.io/gorm"
+)
+
+var routeSet = wire.NewSet(
+	ProvideAuthenticationRoutes,
+)
+
+func ProvideAuthenticationRoutes(routerGroup *gin.RouterGroup, userController user.Controller) *routes.AuthenticationRoutes {
+	authenticationRoutes := routes.NewAuthenticationRoutes(routerGroup, userController)
+	authenticationRoutes.Setup()
+	return authenticationRoutes
+}
+
+var userSet = wire.NewSet(
+	user.NewRepository,
+	wire.Bind(new(user.Repository), new(*user.RepositoryImpl)),
+	user.NewService,
+	wire.Bind(new(user.Service), new(*user.ServiceImpl)),
+	user.NewHandler,
+	wire.Bind(new(user.Controller), new(*user.Handler)),
+)
+
+// wire.go
+func InitializeRoutes(
+	ginRouterGroup *gin.RouterGroup,
+	dbConnection *gorm.DB,
+	validatorInstance *validator.Validate,
+	engTranslator ut.Translator,
+	viperConfig *viper.Viper,
+	mailerService *config.MailerService,
+	identityProvider *config.IdentityProvider,
+) (*routes.ApplicationRoutes, error) {
+	wire.Build(
+		wire.Struct(new(routes.ApplicationRoutes), "*"),
+		routeSet,
+		userSet,
+	)
+	return nil, nil
+}
