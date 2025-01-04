f72ae17a355fc8c97f39a682363a1f3d21a6e719
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-04 23:19:19 +0700

    feat: mapper of user

---
+++ mapper/user_mapper.go
@@ -0,0 +1,31 @@
+package mapper
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-viper/mapstructure/v2"
+	"go-takemikazuchi-api/exception"
+	"go-takemikazuchi-api/helper"
+	"go-takemikazuchi-api/model"
+	"go-takemikazuchi-api/user/dto"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+)
+
+func MapUserDtoIntoUserModel[T *dto.CreateUserDto](userTransferObject T) *model.User {
+	var userModel model.User
+	err := mapstructure.Decode(userTransferObject, &userModel)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userModel.Password), 14)
+	helper.CheckErrorOperation(err, exception.NewClientError(http.StatusBadRequest, exception.ErrBadRequest))
+	userModel.Password = string(hashedPassword)
+	helper.CheckErrorOperation(err, exception.NewClientError(http.StatusBadRequest, exception.ErrBadRequest))
+	return &userModel
+}
+
+func MapJwtClaimIntoUserClaim(jwtClaim jwt.MapClaims) (*dto.JwtClaimDto, error) {
+	var userClaim dto.JwtClaimDto
+	err := mapstructure.Decode(jwtClaim, &userClaim)
+	if err != nil {
+		return nil, err
+	}
+	return &userClaim, nil
+}
