47afb45e926cfb8d86f13e2d18ab5f6362e1aab2
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-06 21:43:16 +0700

    feat: using middleware

--- routes/protected_route.go
+++ routes/protected_route.go
@@ -2,20 +2,28 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"go-takemikazuchi-api/category"
+	"go-takemikazuchi-api/middleware"
 )
 
 type ProtectedRoutes struct {
 	routerGroup        *gin.RouterGroup
 	categoryController category.Controller
+	viperConfig        *viper.Viper
 }
 
-func NewProtectedRoutes(routerGroup *gin.RouterGroup, categoryController category.Controller) *ProtectedRoutes {
+func NewProtectedRoutes(routerGroup *gin.RouterGroup, categoryController category.Controller, viperConfig *viper.Viper) *ProtectedRoutes {
+	routerGroup.Use(middleware.AuthMiddleware(viperConfig))
 	return &ProtectedRoutes{
-		routerGroup:        routerGroup,
+		routerGroup:        routerGroup.Group("categories"),
 		categoryController: categoryController,
+		viperConfig:        viperConfig,
 	}
 }
 
 func (routerGroup *ProtectedRoutes) Setup() {
+	routerGroup.routerGroup.POST("", routerGroup.categoryController.Create)
+	routerGroup.routerGroup.PUT("", routerGroup.categoryController.Update)
+	routerGroup.routerGroup.DELETE("", routerGroup.categoryController.Delete)
 }
