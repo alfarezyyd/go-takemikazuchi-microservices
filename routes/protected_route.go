d0533b76b7428cf3811f12ebc54969a27d5f10e6
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-07 23:07:13 +0700

    feat: setup route for job endpoint

--- routes/protected_route.go
+++ routes/protected_route.go
@@ -4,20 +4,27 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"go-takemikazuchi-api/category"
+	"go-takemikazuchi-api/job"
 	"go-takemikazuchi-api/middleware"
 )
 
 type ProtectedRoutes struct {
 	routerGroup        *gin.RouterGroup
 	categoryController category.Controller
+	jobController      job.Controller
 	viperConfig        *viper.Viper
 }
 
-func NewProtectedRoutes(routerGroup *gin.RouterGroup, categoryController category.Controller, viperConfig *viper.Viper) *ProtectedRoutes {
+func NewProtectedRoutes(routerGroup *gin.RouterGroup,
+	categoryController category.Controller,
+	jobController job.Controller,
+	viperConfig *viper.Viper,
+) *ProtectedRoutes {
 	routerGroup.Use(middleware.AuthMiddleware(viperConfig))
 	return &ProtectedRoutes{
 		routerGroup:        routerGroup.Group("categories"),
 		categoryController: categoryController,
+		jobController:      jobController,
 		viperConfig:        viperConfig,
 	}
 }
@@ -26,4 +33,6 @@ func (routerGroup *ProtectedRoutes) Setup() {
 	routerGroup.routerGroup.POST("", routerGroup.categoryController.Create)
 	routerGroup.routerGroup.PUT("/:id", routerGroup.categoryController.Update)
 	routerGroup.routerGroup.DELETE("/:id", routerGroup.categoryController.Delete)
+
+	routerGroup.routerGroup.POST("", routerGroup.jobController.Create)
 }
