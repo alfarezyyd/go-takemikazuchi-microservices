25ec5b72b0ce1898460164d75b1174ed55a1cea9
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-08 00:38:21 +0700

    feat: transaction relation

--- model/job.go
+++ model/job.go
@@ -3,17 +3,18 @@ package model
 import "time"
 
 type Job struct {
-	ID          uint64     `gorm:"column:id;autoIncrement;primaryKey"`
-	UserId      uint64     `gorm:"column:user_id"`
-	CategoryId  uint64     `gorm:"column:category_id" mapstructure:"CategoryId"`
-	Title       string     `gorm:"column:title" mapstructure:"Title"`
-	Description string     `gorm:"column:description" mapstructure:"Description"`
-	Latitude    float64    `gorm:"column:latitude"`
-	Longitude   float64    `gorm:"column:longitude"`
-	Address     string     `gorm:"column:address"`
-	PlaceId     string     `gorm:"column:place_id"`
-	Price       float64    `gorm:"column:price" mapstructure:"Price"`
-	Status      string     `gorm:"column:status;default:'Open'"`
-	CreatedAt   *time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt   *time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	ID          uint64       `gorm:"column:id;autoIncrement;primaryKey"`
+	UserId      uint64       `gorm:"column:user_id"`
+	CategoryId  uint64       `gorm:"column:category_id" mapstructure:"CategoryId"`
+	Title       string       `gorm:"column:title" mapstructure:"Title"`
+	Description string       `gorm:"column:description" mapstructure:"Description"`
+	Latitude    float64      `gorm:"column:latitude"`
+	Longitude   float64      `gorm:"column:longitude"`
+	Address     string       `gorm:"column:address"`
+	PlaceId     string       `gorm:"column:place_id"`
+	Price       float64      `gorm:"column:price" mapstructure:"Price"`
+	Status      string       `gorm:"column:status;default:'Open'"`
+	CreatedAt   *time.Time   `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt   *time.Time   `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Transaction *Transaction `gorm:"foreignKey:job_id;references:id"`
 }
--- model/transaction.go
+++ model/transaction.go
@@ -1,4 +1,19 @@
 package model
 
+import "time"
+
 type Transaction struct {
+	ID            uint64     `gorm:"column:id;primary_key;autoIncrement"`
+	JobID         uint64     `gorm:"column:job_id"`
+	PayerID       uint64     `gorm:"column:payer_id"`
+	PayeeID       uint64     `gorm:"column:payee_id"`
+	Amount        uint64     `gorm:"column:amount"`
+	SnapToken     string     `gorm:"column:snap_token"`
+	PaymentMethod string     `gorm:"column:payment_method"`
+	Status        string     `gorm:"column:status;default:Pending"`
+	CreatedAt     *time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt     *time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+	Job           *Job       `gorm:"foreignKey:job_id;references:job_id"`
+	PayerUser     *User      `gorm:"foreignKey:payer_id;references:id"`
+	PayeeUser     *User      `gorm:"foreignKey:payee_id;references:id"`
 }
