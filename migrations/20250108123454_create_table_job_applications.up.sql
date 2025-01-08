9c0eaf1a1aefa584dcba5bc58856ce3789b97c91
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-08 19:40:21 +0700

    feat: job application schema

---
+++ migrations/20250108123454_create_table_job_applications.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE job_applications
+(
+    id           BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    job_id       BIGINT UNSIGNED                            NOT NULL,
+    applicant_id BIGINT UNSIGNED                            NOT NULL,
+    status       ENUM ('Pending', 'Rejected', 'Accepted') DEFAULT 'Pending',
+    created_at   TIMESTAMP                                DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP                                DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (job_id) REFERENCES jobs (id),
+    FOREIGN KEY (applicant_id) REFERENCES users (id)
+);
