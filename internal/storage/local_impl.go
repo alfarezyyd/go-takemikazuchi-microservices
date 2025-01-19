6c46623c291183e51226e8c179112047ae926a21
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-19 20:34:38 +0700

    feat local impl of file storage

---
+++ internal/storage/local_impl.go
@@ -0,0 +1,46 @@
+package storage
+
+import (
+	"io"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+)
+
+type LocalStorage struct {
+	BasePath string
+}
+
+// NewLocalStorage creates a new LocalStorage instance.
+func NewLocalStorage(basePath string) *LocalStorage {
+	return &LocalStorage{BasePath: basePath}
+}
+
+// UploadFile saves the file to the local filesystem.
+func (localStorage *LocalStorage) UploadFile(file multipart.File, fileName string) (string, error) {
+	path := filepath.Join(localStorage.BasePath, fileName)
+
+	outFile, err := os.Create(path)
+	if err != nil {
+		return "", err
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, file)
+	if err != nil {
+		return "", err
+	}
+
+	return path, nil
+}
+
+// DeleteFile removes the file from the local filesystem.
+func (localStorage *LocalStorage) DeleteFile(fileName string) error {
+	path := filepath.Join(localStorage.BasePath, fileName)
+	return os.Remove(path)
+}
+
+// GetFileURL returns the local file path.
+func (localStorage *LocalStorage) GetFileURL(fileName string) (string, error) {
+	return filepath.Join(localStorage.BasePath, fileName), nil
+}
