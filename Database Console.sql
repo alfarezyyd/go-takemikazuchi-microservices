de06c999ffc90bc9b40f6efa7c4492d28e28a180
alfarezyyd <adityaalfarezyd@gmail.com>
2025-01-21 14:58:28 +0700

    feat: db console & drop table

--- Database Console.sql
+++ Database Console.sql
@@ -2,7 +2,3 @@ DROP DATABASE IF EXISTS go_takemikazuchi_api;
 CREATE DATABASE go_takemikazuchi_api;
 USE go_takemikazuchi_api;
 
-
-DROP TABLE workers;
-DROP TABLE worker_resources;
-DROP TABLE worker_skills;
