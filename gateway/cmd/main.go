0e6e9456434a140ea89b075bd2223b0b025b46d5
alfarezyyd <adityaalfarezyd@gmail.com>
2025-05-16 16:43:13 +0700

    feat: update

--- gateway/cmd/main.go
+++ gateway/cmd/main.go
@@ -56,7 +56,7 @@ func main() {
 
 	ginEngine := gin.Default()
 	ginEngine.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"http://localhost:3000"}, // atau domain frontend kamu
 		AllowMethods:     []string{"*"},
 		AllowHeaders:     []string{"*"},
 		ExposeHeaders:    []string{"*"},
